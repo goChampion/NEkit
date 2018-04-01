92ad6599d071dbd5f6f28e317c314adf408a8559
goChampion <35716035+goChampion@users.noreply.github.com>
2018-04-01 12:52:49 +0800

    custom edit
    custom edit

--- src/Socket/AdapterSocket/DirectAdapter.swift
+++ src/Socket/AdapterSocket/DirectAdapter.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// This adapter connects to remote directly.
-public class DirectAdapter: AdapterSocket {
+open class DirectAdapter: AdapterSocket {
     /// If this is set to `false`, then the IP address will be resolved by system.
     var resolveHost = false
 
@@ -10,7 +10,7 @@ public class DirectAdapter: AdapterSocket {
 
      - parameter session: The connect session.
      */
-    override public func openSocketWith(session: ConnectSession) {
+    override open func openSocketWith(session: ConnectSession) {
         super.openSocketWith(session: session)
 
         guard !isCancelled else {
@@ -30,18 +30,18 @@ public class DirectAdapter: AdapterSocket {
 
      - parameter socket: The connected socket.
      */
-    override public func didConnectWith(socket: RawTCPSocketProtocol) {
+    override open func didConnectWith(socket: RawTCPSocketProtocol) {
         super.didConnectWith(socket: socket)
         observer?.signal(.readyForForward(self))
         delegate?.didBecomeReadyToForwardWith(socket: self)
     }
 
-    override public func didRead(data: Data, from rawSocket: RawTCPSocketProtocol) {
+    override open func didRead(data: Data, from rawSocket: RawTCPSocketProtocol) {
         super.didRead(data: data, from: rawSocket)
         delegate?.didRead(data: data, from: self)
     }
 
-    override public func didWrite(data: Data?, by rawSocket: RawTCPSocketProtocol) {
+    override open func didWrite(data: Data?, by rawSocket: RawTCPSocketProtocol) {
         super.didWrite(data: data, by: rawSocket)
         delegate?.didWrite(data: data, by: self)
     }
